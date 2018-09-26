cc8dbe9cc08fd6d7e52f0010eb49ba25dbb681de
marcus koh <kohchihao@gmail.com>
2018-09-26 09:50:53 +0800

    Add SignUpCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/SignUpCommandParser.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.SignUpCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Password;
+import seedu.address.model.person.Username;
+import seedu.address.model.user.Manager;
+import seedu.address.model.user.User;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+
+public class SignUpCommandParser implements Parser<SignUpCommand>{
+
+    @Override
+    public SignUpCommand parse(String args) throws ParseException {
+
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_USERNAME, PREFIX_PASSWORD);
+
+        if (!ParserUtil.arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_USERNAME, PREFIX_PASSWORD)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SignUpCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+        Username username = ParserUtil.parseUsername(argMultimap.getValue(PREFIX_USERNAME).get());
+        Password password = ParserUtil.parsePassword(argMultimap.getValue(PREFIX_PASSWORD).get());
+
+        Manager manager = new Manager(name, username, password);
+
+        return new SignUpCommand(manager);
+    }
+}
