b3ec734dc857a0f5dff55ce0fb8f329b8c15059c
marcus koh <kohchihao@gmail.com>
2018-09-26 09:24:07 +0800

    Add User.java and Manager.java

---
+++ src/main/java/seedu/address/model/user/Manager.java
@@ -0,0 +1,23 @@
+package seedu.address.model.user;
+
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Password;
+import seedu.address.model.person.Username;
+
+
+public class Manager extends User{
+
+    /**
+     * Every field must be present and not null.
+     *
+     * @param name
+     * @param username
+     * @param password
+     */
+    public Manager(Name name, Username username, Password password) {
+        super(name, username, password);
+    }
+
+
+}
+
