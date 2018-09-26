7355c5157a7132a4364eb13288deb1e677c146bc
marcus koh <kohchihao@gmail.com>
2018-09-26 09:26:15 +0800

    Add SignUpCommand.java

---
+++ src/main/java/seedu/address/logic/commands/SignUpCommand.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.user.User;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+
+public class SignUpCommand extends Command {
+
+    public static final String COMMAND_WORD = "/signup";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a user to FoodZoom. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_USERNAME + "USERNAME "
+            + PREFIX_PASSWORD + "PASSWORD \n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_USERNAME + "johndoe "
+            + PREFIX_PASSWORD + "johndoepassword";
+
+    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
+    public static final String MESSAGE_DUPLICATE_USER = "This user already exists in FoodZoom.";
+
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+
+        return null;
+    }
+}
