ea8d928ff793c2096e93b4420edb2fae56010c70
Marcus Koh <kohchihao@gmail.com>
2018-09-28 21:33:12 +0800

    XmlFileStorage.java: Add saveUsersDataToFile() and loadUsersDataFromSaveFile()

--- src/main/java/seedu/address/storage/XmlFileStorage.java
+++ src/main/java/seedu/address/storage/XmlFileStorage.java
@@ -7,6 +7,7 @@ import javax.xml.bind.JAXBException;
 
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.util.XmlUtil;
+import seedu.address.storage.user.XmlSerializableUsersList;
 
 /**
  * Stores addressbook data in an XML file
@@ -31,6 +32,30 @@ public class XmlFileStorage {
                                                                             FileNotFoundException {
         try {
             return XmlUtil.getDataFromFile(file, XmlSerializableAddressBook.class);
+        } catch (JAXBException e) {
+            throw new DataConversionException(e);
+        }
+    }
+
+    /**
+     * Saves the given users list data to the specified file.
+     */
+    public static void saveUsersDataToFile(Path file, XmlSerializableUsersList usersList)
+            throws FileNotFoundException {
+        try {
+            XmlUtil.saveDataToFile(file, usersList);
+        } catch (JAXBException e) {
+            throw new AssertionError("Unexpected exception " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Returns users list in the file or an empty users list.
+     */
+    public static XmlSerializableUsersList loadUsersDataFromSaveFile(Path file) throws DataConversionException,
+                                                                                       FileNotFoundException {
+        try {
+            return XmlUtil.getDataFromFile(file, XmlSerializableUsersList.class);
         } catch (JAXBException e) {
             throw new DataConversionException(e);
         }
