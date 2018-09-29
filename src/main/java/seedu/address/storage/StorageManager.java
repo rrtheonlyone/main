75a7b337422e68e8b25700e300b10501a71e2d85
Marcus Koh <kohchihao@gmail.com>
2018-09-29 14:38:28 +0800

    StorageManager.java: Implement handleUsersListChangedEvent() method

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -10,6 +10,7 @@ import com.google.common.eventbus.Subscribe;
 import seedu.address.commons.core.ComponentManager;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
+import seedu.address.commons.events.model.UsersListChangedEvent;
 import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.ReadOnlyAddressBook;
@@ -69,6 +70,16 @@ public class StorageManager extends ComponentManager implements Storage {
     public void saveUsersList(ReadOnlyUsersList usersList, Path filePath) throws IOException {
         logger.fine("Attempting to write to data file: " + filePath);
         usersListStorage.saveUsersList(usersList, filePath);
+    }
+
+    @Override
+    public void handleUsersListChangedEvent(UsersListChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event, "Local data changed, saving to file"));
+        try {
+            saveUsersList(event.data);
+        } catch (IOException e) {
+            raise(new DataSavingExceptionEvent(e));
+        }
     }
 
 
