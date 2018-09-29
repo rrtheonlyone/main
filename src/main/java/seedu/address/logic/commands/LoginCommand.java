aea195515361eacdb578bf96c2cf2987cc9bf788
Marcus Koh <kohchihao@gmail.com>
2018-09-29 17:17:15 +0800

    LoginCommand.java: Update execute() method

--- src/main/java/seedu/address/logic/commands/LoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LoginCommand.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -11,8 +15,33 @@ public class LoginCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Login Success for %1$s";
 
+    public static final String MESSAGE_FAILURE = "Login Failure for %1$s";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Login to FoodZoom. "
+            + "Parameters: "
+            + PREFIX_USERNAME + "USERNAME "
+            + PREFIX_PASSWORD + "PASSWORD \n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_USERNAME + "johndoe "
+            + PREFIX_PASSWORD + "johndoepassword";
+
+    private final Manager toLogin;
+
+    /**
+     * Creates an LoginCommand to add the specified {@code Manager}
+     */
+    public LoginCommand(Manager manager) {
+        requireNonNull(manager);
+        toLogin = manager;
+    }
+
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        return null;
+        requireNonNull(model);
+        if (model.loginUser(toLogin)) {
+            return new CommandResult(String.format(MESSAGE_SUCCESS, toLogin));
+        } else {
+            return new CommandResult(String.format(MESSAGE_FAILURE, toLogin));
+        }
     }
 }
