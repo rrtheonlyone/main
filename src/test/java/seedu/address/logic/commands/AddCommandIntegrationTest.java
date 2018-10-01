86fd87b87d089a8f296cb977fd684f545a34f0d2
Marcus Koh <kohchihao@gmail.com>
2018-10-01 21:02:04 +0800

    AddCommandIntegrationTest.java: Update ModelManager

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.user.TypicalUsers.getTypicalUsersList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,14 +25,14 @@ public class AddCommandIntegrationTest {
 
     @Before
     public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        model = new ModelManager(getTypicalAddressBook(), getTypicalUsersList(), new UserPrefs());
     }
 
     @Test
     public void execute_newPerson_success() {
         Person validPerson = new PersonBuilder().build();
 
-        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(model.getAddressBook(), model.getUsersList(), new UserPrefs());
         expectedModel.addPerson(validPerson);
         expectedModel.commitAddressBook();
 
