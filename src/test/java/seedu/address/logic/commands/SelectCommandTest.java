dca535f9a57678a19444d3a535e26bb27f40bda7
Marcus Koh <kohchihao@gmail.com>
2018-10-01 21:07:55 +0800

    SelectCommandTest.java: Update ModelManager

--- src/test/java/seedu/address/logic/commands/SelectCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SelectCommandTest.java
@@ -10,6 +10,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.user.TypicalUsers.getTypicalUsersList;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,8 +31,8 @@ public class SelectCommandTest {
     @Rule
     public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model model = new ModelManager(getTypicalAddressBook(), getTypicalUsersList(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalUsersList(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
