66fe93e658a115593fe594fa470eeed6de80b3ac
Monika <manuela.monika97@gmail.com>
2018-10-02 03:01:14 +0800

    Add XML storage files for Route

--- src/main/java/seedu/address/storage/XmlFileStorage.java
+++ src/main/java/seedu/address/storage/XmlFileStorage.java
@@ -7,6 +7,7 @@ import javax.xml.bind.JAXBException;
 
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.util.XmlUtil;
+import seedu.address.storage.route.XmlSerializableRouteList;
 
 /**
  * Stores addressbook data in an XML file
@@ -25,12 +26,36 @@ public class XmlFileStorage {
     }
 
     /**
-     * Returns address book in the file or an empty address book
+     * Returns address book in the file or an empty address book.
      */
     public static XmlSerializableAddressBook loadDataFromSaveFile(Path file) throws DataConversionException,
                                                                             FileNotFoundException {
         try {
             return XmlUtil.getDataFromFile(file, XmlSerializableAddressBook.class);
+        } catch (JAXBException e) {
+            throw new DataConversionException(e);
+        }
+    }
+
+    /**
+     * Saves the given route list data to the specified file.
+     */
+    public static void saveRouteDataToFile(Path file, XmlSerializableRouteList routeList)
+            throws FileNotFoundException {
+        try {
+            XmlUtil.saveDataToFile(file, routeList);
+        } catch (JAXBException e) {
+            throw new AssertionError("Unexpected exception " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Returns route list in the file or an empty route list.
+     */
+    public static XmlSerializableRouteList loadRouteDataFromSaveFile(Path file) throws DataConversionException,
+            FileNotFoundException {
+        try {
+            return XmlUtil.getDataFromFile(file, XmlSerializableRouteList.class);
         } catch (JAXBException e) {
             throw new DataConversionException(e);
         }
