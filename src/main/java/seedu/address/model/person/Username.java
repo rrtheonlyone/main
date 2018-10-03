b88f13e2e3d6ee9235b764de40c160e65384e413
Marcus Koh <kohchihao@gmail.com>
2018-10-03 19:27:45 +0800

    Username.java: Update regex for username

--- src/main/java/seedu/address/model/person/Username.java
+++ src/main/java/seedu/address/model/person/Username.java
@@ -15,7 +15,7 @@ public class Username {
      * The first character of the username must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String USERNAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum}]*";
+    public static final String USERNAME_VALIDATION_REGEX = "[\\p{Alnum}]+";
     public final String value;
 
 
