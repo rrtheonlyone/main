04d546f6464307ab860531b6f8d6a438fedfd24a
Marcus Koh <kohchihao@gmail.com>
2018-10-08 23:28:03 +0800

    User.java: Remove toString() and hashCode()

--- src/main/java/seedu/address/model/user/User.java
+++ src/main/java/seedu/address/model/user/User.java
@@ -75,42 +75,6 @@ public abstract class User {
     }
 
 
-    /**
-     * Returns true if both persons have the same identity and data fields.
-     * This defines a stronger notion of equality between two persons.
-     */
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
 
-        if (!(other instanceof User)) {
-            return false;
-        }
-
-        User otherPerson = (User) other;
-        return otherPerson.getName().equals(getName())
-                && otherPerson.getUsername().equals(getUsername())
-                && otherPerson.getPassword().equals(getPassword());
-    }
-
-    @Override
-    public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, username, password);
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(getName())
-                .append(" Username: ")
-                .append(getUsername())
-                .append(" Password: ")
-                .append(getPassword());
-
-        return builder.toString();
-    }
 
 }
