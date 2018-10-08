3c73d0f22e3091caf09bd381e5fe90673809fd15
Marcus Koh <kohchihao@gmail.com>
2018-10-08 17:48:41 +0800

    UserSession.java: Add javadocs

--- src/main/java/seedu/address/model/user/UserSession.java
+++ src/main/java/seedu/address/model/user/UserSession.java
@@ -7,34 +7,50 @@ public class UserSession {
     private User loggedInUser;
     private boolean isLoggedIn;
 
+    /**
+     * Initializes an empty UserSession.
+     */
     public UserSession() {
+        isLoggedIn = false;
     }
 
+    /**
+     * Initializes a UserSession with user.
+     * @param toLogin User that has successfully logged in.
+     */
     public UserSession(User toLogin) {
         loggedInUser = toLogin;
         this.isLoggedIn = true;
     }
 
+    /**
+     * Set up user session.
+     * @param toLogin User that has successfully logged in.
+     */
     public void setUserSession(User toLogin) {
         this.loggedInUser = toLogin;
         this.isLoggedIn = true;
     }
 
+    /**
+     * Check if user is already logged into FoodZoom.
+     */
     public boolean isUserAlreadyLoggedIn() {
         return this.isLoggedIn;
     }
 
-    public void logOutUser() {
+    /**
+     * To clear user session details.
+     */
+    public void logoutUser() {
         this.isLoggedIn = false;
+        this.loggedInUser = null;
     }
 
+    /**
+     * Return logged in user details.
+     */
     public User getLoggedInUserDetails() {
         return this.loggedInUser;
     }
-
-
-
-
-
-
 }
