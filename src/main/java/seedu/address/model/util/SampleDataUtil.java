181397f84ecb97c95d14c182a14344113724ddb8
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-10-09 15:49:09 +0800

    Update Sample Dataset for order

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,6 +1,7 @@
 package seedu.address.model.util;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,7 @@ import seedu.address.model.deliveryman.Deliveryman;
 import seedu.address.model.deliveryman.DeliverymenList;
 import seedu.address.model.order.Food;
 import seedu.address.model.order.Order;
+import seedu.address.model.order.OrderDate;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Password;
@@ -27,22 +29,22 @@ public class SampleDataUtil {
     public static Order[] getSampleOrders() {
         return new Order[]{
             new Order(new Name("Alex Yeoh"), new Phone("87438807"),
-                        new Address("Blk 30 Geylang Street 29, #06-40"),
+                        new Address("Blk 30 Geylang Street 29, #06-40"), new OrderDate("01-10-2018 10:00:00"),
                         getFoodSet("Ice Tea")),
             new Order(new Name("Bernice Yu"), new Phone("99272758"),
-                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new OrderDate("01-10-2018 10:00:00"),
                         getFoodSet("Chicken Rice")),
             new Order(new Name("Charlotte Oliveiro"), new Phone("93210283"),
-                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new OrderDate("01-10-2018 10:00:00"),
                         getFoodSet("Nasi Goreng")),
             new Order(new Name("David Li"), new Phone("91031282"),
-                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new OrderDate("01-10-2018 10:00:00"),
                         getFoodSet("Satay")),
             new Order(new Name("Irfan Ibrahim"), new Phone("92492021"),
-                        new Address("Blk 47 Tampines Street 20, #17-35"),
+                        new Address("Blk 47 Tampines Street 20, #17-35"), new OrderDate("01-10-2018 10:00:00"),
                         getFoodSet("Fish and Chips")),
             new Order(new Name("Roy Balakrishnan"), new Phone("92624417"),
-                        new Address("Blk 45 Aljunied Street 85, #11-31"),
+                        new Address("Blk 45 Aljunied Street 85, #11-31"), new OrderDate("01-10-2018 10:00:00"),
                         getFoodSet("Sugercane Juice"))
         };
     }
