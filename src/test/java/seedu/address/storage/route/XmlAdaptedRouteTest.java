d882948e59a641072c47086f1ec3610f3266a37a
Monika <manuela.monika97@gmail.com>
2018-10-12 17:05:34 +0800

    Modify Route storage tests

--- src/test/java/seedu/address/storage/route/XmlAdaptedRouteTest.java
+++ src/test/java/seedu/address/storage/route/XmlAdaptedRouteTest.java
@@ -1,50 +1,53 @@
 package seedu.address.storage.route;
 
 import static org.junit.Assert.assertEquals;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_ANGMOKIO;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
 import static seedu.address.storage.route.XmlAdaptedRoute.MISSING_FIELD_MESSAGE_FORMAT;
-import static seedu.address.testutil.TypicalRoutes.ANGMOKIO;
+import static seedu.address.testutil.TypicalRoutes.ROUTE_ALICE_BENSON;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.order.Order;
 import seedu.address.model.person.Address;
+import seedu.address.storage.XmlAdaptedOrder;
 import seedu.address.testutil.Assert;
+import seedu.address.testutil.TypicalRoutes;
 
 public class XmlAdaptedRouteTest {
     private static final String INVALID_ADDRESS = " ";
+    private static final List<XmlAdaptedOrder> VALID_ORDERS = TypicalRoutes.ROUTE_ALICE_BENSON.getOrders().stream()
+            .map(XmlAdaptedOrder::new)
+            .collect(Collectors.toList());
 
     @Test
-    public void toModelType_validRouteDetails_returnsPerson() throws Exception {
-        XmlAdaptedRoute route = new XmlAdaptedRoute(ANGMOKIO);
-        assertEquals(ANGMOKIO, route.toModelType());
+    public void toModelType_validRouteDetails_returnsRoute() throws Exception {
+        XmlAdaptedRoute route = new XmlAdaptedRoute(ROUTE_ALICE_BENSON);
+        assertEquals(ROUTE_ALICE_BENSON, route.toModelType());
     }
 
     @Test
     public void toModelType_invalidSource_throwsIllegalValueException() {
-        XmlAdaptedRoute route = new XmlAdaptedRoute(INVALID_ADDRESS, VALID_ADDRESS_ANGMOKIO);
-        String expectedMessage = Address.MESSAGE_ADDRESS_CONSTRAINTS;
-        Assert.assertThrows(IllegalValueException.class, expectedMessage, route::toModelType);
-    }
-
-    @Test
-    public void toModelType_invalidDestination_throwsIllegalValueException() {
-        XmlAdaptedRoute route = new XmlAdaptedRoute(VALID_ADDRESS_ANGMOKIO, INVALID_ADDRESS);
+        XmlAdaptedRoute route = new XmlAdaptedRoute(INVALID_ADDRESS, VALID_ORDERS);
         String expectedMessage = Address.MESSAGE_ADDRESS_CONSTRAINTS;
         Assert.assertThrows(IllegalValueException.class, expectedMessage, route::toModelType);
     }
 
     @Test
     public void toModelType_nullSource_throwsIllegalValueException() {
-        XmlAdaptedRoute route = new XmlAdaptedRoute((String) null, VALID_ADDRESS_ANGMOKIO);
+        XmlAdaptedRoute route = new XmlAdaptedRoute(null, VALID_ORDERS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName());
         Assert.assertThrows(IllegalValueException.class, expectedMessage, route::toModelType);
     }
 
     @Test
-    public void toModelType_nullDestination_throwsIllegalValueException() {
-        XmlAdaptedRoute route = new XmlAdaptedRoute(VALID_ADDRESS_ANGMOKIO, (String) null);
-        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName());
+    public void toModelType_nullOrders_throwsIllegalValueException() {
+        XmlAdaptedRoute route = new XmlAdaptedRoute(VALID_ADDRESS_AMY, null);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Order.class.getSimpleName());
         Assert.assertThrows(IllegalValueException.class, expectedMessage, route::toModelType);
     }
 
