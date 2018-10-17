102504dfd78e9784244e72bdacce8b083f5348c8
Marcus Koh <kohchihao@gmail.com>
2018-10-17 14:23:13 +0800

    SignUpCommandSystemTest.java: Update test to match

--- src/test/java/systemtests/SignUpCommandSystemTest.java
+++ src/test/java/systemtests/SignUpCommandSystemTest.java
@@ -4,11 +4,16 @@ import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_PASSWORD_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_USERNAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_NAME_ALICE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_NAME_HOON;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_NAME_IDA;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_PASSWORD_ALICE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_PASSWORD_HOON;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_PASSWORD_IDA;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_USERNAME_ALICE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_USERNAME_HOON;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_USERNAME_IDA;
+import static seedu.address.logic.commands.LoginCommand.MESSAGE_ALREADY_LOGGED_IN;
+import static seedu.address.logic.commands.LoginCommand.MESSAGE_REDIRECT_TO_LOGOUT;
 import static seedu.address.logic.commands.SignUpCommand.MESSAGE_DUPLICATE_USER;
 import static seedu.address.logic.commands.SignUpCommand.MESSAGE_LOGGED_IN;
 import static seedu.address.logic.commands.SignUpCommand.MESSAGE_SUCCESS;
@@ -29,6 +34,42 @@ import seedu.address.testutil.user.UserBuilder;
 public class SignUpCommandSystemTest extends OrderBookSystemTest {
 
     private String signupCommand = SignUpCommand.COMMAND_WORD + " ";
+
+    @Test
+    public void cannotSignupAfterSignup() {
+        Model expectedModel = getModel();
+        String command = signupCommand + PREFIX_NAME + VALID_MANAGER_NAME_IDA
+                + " " + PREFIX_USERNAME + VALID_MANAGER_USERNAME_IDA
+                + " " + PREFIX_PASSWORD + VALID_MANAGER_PASSWORD_IDA;
+
+        User ida = new UserBuilder()
+                .withName(VALID_MANAGER_NAME_IDA)
+                .withUsername(VALID_MANAGER_USERNAME_IDA)
+                .withPassword(VALID_MANAGER_PASSWORD_IDA)
+                .build();
+
+        String expectedResultMessage = String.format(MESSAGE_SUCCESS, ida)
+                + "\n"
+                + MESSAGE_LOGGED_IN;
+
+
+        assertCommandSuccess(command, expectedModel, expectedResultMessage);
+
+        command = signupCommand + PREFIX_NAME + VALID_MANAGER_NAME_HOON
+                + " " + PREFIX_USERNAME + VALID_MANAGER_USERNAME_HOON
+                + " " + PREFIX_PASSWORD + VALID_MANAGER_PASSWORD_HOON;
+
+        User hoon = new UserBuilder()
+                .withName(VALID_MANAGER_NAME_HOON)
+                .withUsername(VALID_MANAGER_USERNAME_HOON)
+                .withPassword(VALID_MANAGER_PASSWORD_HOON)
+                .build();
+
+        expectedResultMessage = String.format(MESSAGE_ALREADY_LOGGED_IN, ida.getUsername())
+                + "\n"
+                + MESSAGE_REDIRECT_TO_LOGOUT;
+        assertCommandSuccess(command, expectedModel, expectedResultMessage);
+    }
 
     @Test
     public void signup_newUser_successful() {
