c12027b7bd5ced42f79eaf7eb46ffe20de48e4aa
Marcus Koh <kohchihao@gmail.com>
2018-10-17 14:01:03 +0800

    Update LoginCommand.java to prevent user from logging in again

--- src/test/java/systemtests/LoginCommandSystemTest.java
+++ src/test/java/systemtests/LoginCommandSystemTest.java
@@ -2,8 +2,10 @@ package systemtests;
 
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_PASSWORD_ALICE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_PASSWORD_BENSON;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_PASSWORD_HOON;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_PASSWORD_KENNY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_USERNAME_ALICE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_USERNAME_HOON;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MANAGER_USERNAME_KENNY;
 import static seedu.address.logic.commands.LoginCommand.MESSAGE_FAILURE;
 import static seedu.address.logic.commands.LoginCommand.MESSAGE_SUCCESS;
@@ -16,6 +18,27 @@ import seedu.address.logic.commands.LoginCommand;
 import seedu.address.model.Model;
 
 public class LoginCommandSystemTest extends OrderBookSystemTest {
+
+
+    @Test
+    public void cannotLoginAfterLogin() {
+        Model expectedModel = getModel();
+        String loginCommand = LoginCommand.COMMAND_WORD + " ";
+
+        /* Case: Successful login, correct username and password */
+        String command = loginCommand + PREFIX_USERNAME + VALID_MANAGER_USERNAME_ALICE
+                + " " + PREFIX_PASSWORD + VALID_MANAGER_PASSWORD_ALICE;
+        String expectedResultMessage = String.format(
+                MESSAGE_SUCCESS, "Username: " + VALID_MANAGER_USERNAME_ALICE);
+        assertCommandSuccess(command, expectedModel, expectedResultMessage);
+
+        /* Case: Attempt to login after successful login */
+        command = loginCommand + PREFIX_USERNAME + VALID_MANAGER_USERNAME_HOON
+                + " " + PREFIX_PASSWORD + VALID_MANAGER_PASSWORD_HOON;
+        expectedResultMessage = String.format(LoginCommand.MESSAGE_ALREADY_LOGGED_IN,
+                "Username: " + VALID_MANAGER_USERNAME_ALICE) + "\n" + LoginCommand.MESSAGE_REDIRECT_TO_LOGOUT;
+        assertCommandSuccess(command, expectedModel, expectedResultMessage);
+    }
 
     @Test
     public void login() {
