9ca7e22865bb70d8d507babda64f0bdefc45520a
Marcus Koh <kohchihao@gmail.com>
2018-10-20 15:12:18 +0800

    MainWindow.java: Update imports

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -7,7 +7,6 @@ import com.google.common.eventbus.Subscribe;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
