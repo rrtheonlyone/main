86cb3c37cdd76fa55ef7325fe5556c6e208f5b86
Jin Ying <developer.tjy@gmail.com>
2018-10-20 22:06:13 +0800

    OrderPhoneContainsKeywordPredicate.java: Change phone matching mechanism

--- src/main/java/seedu/address/model/order/OrderPhoneContainsKeywordPredicate.java
+++ src/main/java/seedu/address/model/order/OrderPhoneContainsKeywordPredicate.java
@@ -1,34 +1,29 @@
 package seedu.address.model.order;
 
+import java.util.List;
+
 import java.util.function.Predicate;
 
 /**
  * Tests that a {@code Order}'s {@code Phone} matches any of the keywords given.
  */
 public class OrderPhoneContainsKeywordPredicate implements Predicate<Order> {
-    private final String keyword;
+    private final List<String> keywords;
 
-    public OrderPhoneContainsKeywordPredicate(String phone) {
-        phone = removeAllWhiteSpaces(phone);
-        keyword = phone;
+    public OrderPhoneContainsKeywordPredicate(List<String> phone) {
+        keywords = phone;
     }
 
     @Override
     public boolean test(Order order) {
-        return order.getPhone().value.contains(keyword);
+        return keywords.stream()
+                .anyMatch(keyword -> order.getPhone().value.contains(keyword));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof OrderPhoneContainsKeywordPredicate // instanceof handles nulls
-                && keyword.equals(((OrderPhoneContainsKeywordPredicate) other).keyword)); // state check
-    }
-
-    /**
-     * Remove all trailing and leading whitespace and whitespaces in between {@code input}
-     */
-    private String removeAllWhiteSpaces(String input) {
-        return input.trim().replaceAll("\\s+", "");
+                && keywords.equals(((OrderPhoneContainsKeywordPredicate) other).keywords)); // state check
     }
 }
