f7d0c9e58bd17f223de0a5a955786c47c3f19034
Jin Ying <developer.tjy@gmail.com>
2018-10-22 22:27:55 +0800

    OrderDatePredicate.java: Takes in a List<Date> instead of List<String>

--- src/main/java/seedu/address/model/order/OrderDatePredicate.java
+++ src/main/java/seedu/address/model/order/OrderDatePredicate.java
@@ -1,14 +1,8 @@
 package seedu.address.model.order;
 
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 
 /**
  * Tests that a {@code Order}'s {@code Date} matches any of the keywords given.
@@ -18,22 +12,19 @@ public class OrderDatePredicate implements Predicate<Order> {
     public static final int LOWER_DATE_RANGE_INDEX = 0;
     public static final int UPPER_DATE_RANGE_INDEX = 1;
 
-    private static final SimpleDateFormat sf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-
     private Predicate<Order> datePredicate;
 
-    public OrderDatePredicate(List<String> stringDates) {
-        List<Date> dates = stringDates.stream().map(stringDate -> parseDate(stringDate)).collect(Collectors.toList());
+    public OrderDatePredicate(List<Date> dates) {
+        assert dates.size() < 3;
 
-        if (stringDates.size() == VALID_SEARCH_DATE_RANGE_SIZE) {
+        if (dates.size() == VALID_SEARCH_DATE_RANGE_SIZE) {
             Date lowerDateBoundary = dates.get(LOWER_DATE_RANGE_INDEX);
             Date upperDateBoundary = dates.get(UPPER_DATE_RANGE_INDEX);
             assert (lowerDateBoundary.before(upperDateBoundary) && lowerDateBoundary != upperDateBoundary);
             datePredicate = new OrderDateRangePredicate(lowerDateBoundary, upperDateBoundary);
         } else {
-            // take the last date in the list
-            int lastIndex = dates.size() - 1;
-            datePredicate = new OrderDateSinglePredicate(dates.get(lastIndex));
+            // One date in list
+            datePredicate = new OrderDateSinglePredicate(dates.get(0));
         }
     }
 
@@ -47,17 +38,5 @@ public class OrderDatePredicate implements Predicate<Order> {
         return other == this // short circuit if same object
                 || (other instanceof OrderDatePredicate // instanceof handles nulls
                 && datePredicate.equals(((OrderDatePredicate) other).datePredicate)); // state check
-    }
-
-    /**
-     * Parses {@code stringDate} to a Date object
-     */
-    private Date parseDate(String stringDate) {
-        try {
-            return sf.parse(stringDate);
-        } catch (ParseException pE) {
-            checkArgument(false, OrderDate.MESSAGE_DATE_CONSTRAINTS);
-            return null;
-        }
     }
 }
