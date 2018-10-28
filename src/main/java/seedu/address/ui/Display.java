37f8ecd1851f9b2d2b3e621bdb1fafe1e8ef452c
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-10-28 16:01:25 +0800

    Update methods to load placeholders onto map based on postal code

--- src/main/java/seedu/address/ui/Display.java
+++ src/main/java/seedu/address/ui/Display.java
@@ -1,19 +1,121 @@
 package seedu.address.ui;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
+import com.google.common.eventbus.Subscribe;
+
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.OrderPanelSelectionChangedEvent;
+import seedu.address.model.order.Order;
+import seedu.address.model.order.OrderStatus;
 
 /**
- * Panel containing the list of orders.
+ * Panel containing the main display - map and side content.
  */
 public class Display extends UiPart<Region> {
 
     private static final String FXML = "Display.fxml";
     private final Logger logger = LogsCenter.getLogger(Display.class);
 
-    public Display() {
+    @FXML
+    private StackPane mapWrapper;
+
+    private MapPanel mapPanel;
+    private ObservableList<Order> orderList;
+    private HashMap<String, Integer> directory;
+
+    public Display(ObservableList<Order> orderList) {
         super(FXML);
+        this.orderList = orderList;
+
+        fillInnerParts();
+        setupMap();
+
+        this.orderList.addListener((ListChangeListener.Change<? extends Order> change) -> {
+            while (change.next()) {
+                if (change.wasUpdated()) {
+
+                    directory = new HashMap<>();
+                    for (Order o : change.getList()) {
+                        if (o.getOrderStatus().toString().equals("PENDING")) {
+                            String postalCode = o.getAddress().getPostalCode();
+                            if (directory.containsKey(postalCode)) {
+                                directory.put(postalCode, directory.get(postalCode) + 1);
+                            } else {
+                                directory.put(postalCode, 1);
+                            }
+                        }
+                    }
+
+                    mapPanel.clear();
+                    mapPanel.initialise(directory);
+
+                } else {
+                    for (Order o : change.getRemoved()) {
+                        logger.info("REMOVED " + o.toString());
+                        if (o.getOrderStatus().toString().equals("PENDING")) {
+                            String postalCode = o.getAddress().getPostalCode();
+                            if (directory.containsKey(postalCode)) {
+                                if (directory.get(postalCode) <= 1) {
+                                    directory.remove(postalCode);
+                                } else {
+                                    directory.put(postalCode, directory.get(postalCode) - 1);
+                                }
+                            }
+                        }
+                    }
+
+                    for (Order o : change.getAddedSubList()) {
+                        logger.info("ADDED " + o.toString());
+                        if (o.getOrderStatus().toString().equals("PENDING")) {
+                            String postalCode = o.getAddress().getPostalCode();
+                            if (directory.containsKey(postalCode)) {
+                                directory.put(postalCode, directory.get(postalCode) + 1);
+                            } else {
+                                directory.put(postalCode, 1);
+                            }
+                        }
+                    }
+
+                    mapPanel.clear();
+                    mapPanel.initialise(directory);
+                }
+            }
+        });
+
+        registerAsAnEventHandler(this);
+    }
+
+    private void fillInnerParts() {
+        mapPanel = new MapPanel();
+        mapWrapper.getChildren().add(mapPanel.getRoot());
+    }
+
+    private void setupMap() {
+        directory = new HashMap<>();
+        for (Order o : orderList) {
+            // map all pending orders
+            if (o.getOrderStatus().toString().equals("PENDING")) {
+                String postalCode = o.getAddress().getPostalCode();
+                if (directory.containsKey(postalCode)) {
+                    directory.put(postalCode, directory.get(postalCode) + 1);
+                } else {
+                    directory.put(postalCode, 1);
+                }
+            }
+        }
+
+        mapPanel.initialise(directory);
+    }
+
+    @Subscribe
+    private void handleOrderPanelSelectionChangedEvent(OrderPanelSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
     }
 }
--- src/main/java/seedu/address/ui/MapPanel.java
+++ src/main/java/seedu/address/ui/MapPanel.java
@@ -1,9 +1,15 @@
 package seedu.address.ui;
 
+import static seedu.address.ui.util.MapDataUtil.DISTRICT_CACHE;
+import static seedu.address.ui.util.MapDataUtil.POSTAL_CODE_CACHE;
+
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
@@ -15,13 +21,51 @@ import seedu.address.model.order.Order;
 public class MapPanel extends UiPart<Region> {
 
     private static final String FXML = "MapPanel.fxml";
+    private static final String PLACEHOLDER_IMAGE_PATH = "/images/receipt.png";
+
     private final Logger logger = LogsCenter.getLogger(MapPanel.class);
+
+    private final Image placeholder = new Image(getClass().getResource(PLACEHOLDER_IMAGE_PATH).toExternalForm());
+    private ImageView placeholderContainer;
 
     @FXML
     private GridPane mapPane;
 
-    public MapPanel(ObservableList<Order> orderList) {
+    public MapPanel() {
         super(FXML);
     }
 
+    /**
+     * Initialises map with all pending orders using a cache
+     * @param orderMap map of pending orders where key is postal code and value is counter
+     */
+    public void initialise(HashMap<String, Integer> orderMap) {
+        // for each postal code
+        for (String code : orderMap.keySet()) {
+
+            String postalCodeKey = code.substring(0,2);
+            if (POSTAL_CODE_CACHE.containsKey(postalCodeKey)) {
+                int regionCode = POSTAL_CODE_CACHE.get(postalCodeKey);
+                Integer[] coordinates = DISTRICT_CACHE.get(regionCode);
+
+                placeholderContainer = new ImageView(placeholder);
+
+                int count = orderMap.get(code);
+                int increment = 16 + count * 2;
+
+                placeholderContainer.setPreserveRatio(true);
+                placeholderContainer.setFitHeight(increment);
+                placeholderContainer.setFitWidth(increment);
+
+                mapPane.add(placeholderContainer, coordinates[1], coordinates[0]);
+            }
+        }
+    }
+
+    /**
+     * Clears all placeholders from map
+     */
+    public void clear() {
+        mapPane.getChildren().clear();
+    }
 }
