7a22c2861a557ddc88905a5154b2108b8f986352
Marcus Koh <kohchihao@gmail.com>
2018-11-03 21:06:06 +0800

    DoneCommandTest.java: Add suitable test

--- src/test/java/seedu/address/logic/commands/order/DoneCommandTest.java
+++ src/test/java/seedu/address/logic/commands/order/DoneCommandTest.java
@@ -1,8 +1,7 @@
 package seedu.address.logic.commands.order;
 
+import static org.junit.Assert.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalDeliverymen.CHIKAO;
 import static seedu.address.testutil.TypicalDeliverymen.getTypicalDeliverymenList;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 import static seedu.address.testutil.TypicalOrders.ALICE;
@@ -15,16 +14,17 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.AssignCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.deliveryman.Deliveryman;
 import seedu.address.model.order.Order;
-import seedu.address.testutil.DeliverymanBuilder;
 import seedu.address.testutil.OrderBuilder;
 
 public class DoneCommandTest {
@@ -42,47 +42,44 @@ public class DoneCommandTest {
 
         Order expectedOrder = new OrderBuilder(ALICE).withStatus("COMPLETED").build();
 
-        Model expectedModel = new ModelManager(model.getOrderBook(), model.getUsersList(),
-                model.getDeliverymenList(), new UserPrefs());
-
-        //get ongoing order.
-        Order orderTobeCompleted = model.getFilteredOrderList().get(0);
-        Deliveryman deliveryman = new DeliverymanBuilder(CHIKAO).build();
-        deliveryman.addOrder(orderTobeCompleted);
-
-        //mark order as completed.
-        Order completedOrder = new OrderBuilder(orderTobeCompleted).build();
-        completedOrder.setStatusCompleted();
-
-        //remove order from deliveryman
-        Deliveryman updatedDeliveryman = orderTobeCompleted.getDeliveryman();
-        updatedDeliveryman.removeOrder(orderTobeCompleted);
-
-        expectedModel.updateOrder(orderTobeCompleted, completedOrder);
-        expectedModel.commitOrderBook();
-        expectedModel.updateDeliveryman(deliveryman, updatedDeliveryman);
-        expectedModel.commitDeliverymenList();
-
-        assertExecutionSuccess(INDEX_FIRST, expectedOrder, expectedModel);
-    }
-
-    /**
-     * Executes a {@code DoneCommand} with the given {@code index}, and checks that {@code JumpToListRequestEvent}
-     * is raised with the correct index.
-     */
-    private void assertExecutionSuccess(Index index, Order expectedOrder, Model expectedModel) {
-        DoneCommand doneCommand = new DoneCommand(index);
         String expectedMessage = String.format(DoneCommand.MESSAGE_COMPLETED_ORDER_SUCCESS, expectedOrder);
+        DoneCommand doneCommand = new DoneCommand(INDEX_FIRST);
+        assertCommandSuccess(doneCommand, model, commandHistory, expectedMessage);
+    }
 
-        assertCommandSuccess(doneCommand, model, commandHistory, expectedMessage, expectedModel);
+    @Test
+    public void execute_invalidIndexUnfliteredList_failure() throws CommandException {
+        AssignCommand assignCommand = new AssignCommand(INDEX_FIRST, ordersSet);
+        assignCommand.execute(model, commandHistory);
+
+        Index last = Index.fromZeroBased(model.getFilteredOrderList().size());
+        DoneCommand doneCommand = new DoneCommand(last);
+
+        String expectedMessage = Messages.MESSAGE_INVALID_ORDER_DISPLAYED_INDEX;
+        assertCommandFailure(doneCommand, model, commandHistory, expectedMessage);
+    }
+
+    @Test
+    public void execute_validIndexUnfliteredList_pendingFailure() throws CommandException {
+        Index last = Index.fromZeroBased(2);
+        DoneCommand doneCommand = new DoneCommand(last);
+
+        String expectedMessage = DoneCommand.MESSAGE_ONGOING_ORDER;
+        assertCommandFailure(doneCommand, model, commandHistory, expectedMessage);
     }
 
     /**
-     * Executes a {@code SelectCommand} with the given {@code index}, and checks that a {@code CommandException}
-     * is thrown with the {@code expectedMessage}.
+     * Check if the command runs successfully.
      */
-    private void assertExecutionFailure(Index index, String expectedMessage) {
-        SelectCommand selectCommand = new SelectCommand(index);
-        assertCommandFailure(selectCommand, model, commandHistory, expectedMessage);
+    public static void assertCommandSuccess(Command command, Model actualModel, CommandHistory actualCommandHistory,
+                                            String expectedMessage) {
+        CommandHistory expectedCommandHistory = new CommandHistory(actualCommandHistory);
+        try {
+            CommandResult result = command.execute(actualModel, actualCommandHistory);
+            assertEquals(expectedMessage, result.feedbackToUser);
+            assertEquals(expectedCommandHistory, actualCommandHistory);
+        } catch (CommandException ce) {
+            throw new AssertionError("Execution of command should not fail.", ce);
+        }
     }
 }
